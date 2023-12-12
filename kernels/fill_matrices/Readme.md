b053ddd73d98d3fb09bf3c4d7157b559823d873a
Mark Dewing <markdewing@gmail.com>
2023-12-12 16:13:08 -0600

    Add cache-blocked version of the fill function

--- kernels/fill_matrices/Readme.md
+++ kernels/fill_matrices/Readme.md
@@ -4,3 +4,10 @@ Extract fillOverlapHamiltonianMatrices function from QMCCostFunctionBatched
 
 The loop structure and memory accesses are similar to a matrix multiply.
 
+Use the "fill\_matrices" parameter to optimizer to output the inputs for this function.
+(This parameter is only implemented in the "rot_det_speedup" branch, in this commit: https://github.com/markdewing/qmcpack/commit/dd92ad1454d8636704fcfc33fd974f933b19c57e )
+
+The files will be have the suffix "param_deriv.h5".
+
+The executable can optionally take the corresponding linear matrices file as a second parameter
+and this will allow the function output to be verified.
--- kernels/fill_matrices/fill_matrices.cpp
+++ kernels/fill_matrices/fill_matrices.cpp
@@ -1,7 +1,7 @@
 
-// Compute overlap and hamiltonian matrices from parameter derivative data stored
-// in *.param_deriv.h5 files
-// And optionally compare the results with the matrices stored in *.linear_matrices.h5
+// Compute overlap and hamiltonian matrices from parameter derivative data
+// stored in *.param_deriv.h5 files And optionally compare the results with the
+// matrices stored in *.linear_matrices.h5
 
 #include <algorithm>
 #include <cassert>
@@ -86,6 +86,146 @@ enum SumIndex_OPT {
   SUM_WGTSQ,
   SUM_INDEX_SIZE
 };
+
+void fillOverlapHamiltonianMatricesBlocked(
+    const int nsamples, const int numParams, std::vector<double> SumValue,
+    const Matrix &RecordsOnNode_, const Matrix &DerivRecords_,
+    const Matrix &HDerivRecords_, const double w_beta, Matrix &Left,
+    Matrix &Right) {
+  RealType b2;
+  b2 = w_beta;
+  const int rank_local_num_samples_ = nsamples;
+
+  // Right               = 0.0;
+  // Left                = 0.0;
+  double curAvg_w = SumValue[SUM_E_WGT] / SumValue[SUM_WGT];
+  Return_rt curAvg2_w = SumValue[SUM_ESQ_WGT] / SumValue[SUM_WGT];
+  RealType V_avg = curAvg2_w - curAvg_w * curAvg_w;
+  std::vector<Return_t> D_avg(numParams, 0.0);
+  Return_rt wgtinv = 1.0 / SumValue[SUM_WGT];
+
+  std::chrono::system_clock::time_point first_loop_start =
+      std::chrono::system_clock::now();
+
+  for (int iw = 0; iw < rank_local_num_samples_; iw++) {
+    Return_rt weight = RecordsOnNode_(iw, REWEIGHT) * wgtinv;
+    for (int pm = 0; pm < numParams; pm++) {
+      D_avg[pm] += DerivRecords_(iw, pm) * weight;
+    }
+  }
+  std::chrono::system_clock::time_point first_loop_end =
+      std::chrono::system_clock::now();
+  std::chrono::duration<double> first_loop_time =
+      first_loop_end - first_loop_start;
+  std::cout << "  First loop in fill time: " << first_loop_time.count()
+            << std::endl;
+
+  // myComm->allreduce(D_avg);
+  //
+  // Block size for samples
+  const int sbs = 32;
+  // Block size or parameters
+  const int pbs = 32;
+
+  double local_left_data[pbs * pbs];
+  double local_right_data[pbs * pbs];
+  double local_left_vec1[pbs];
+  double local_left_vec2[pbs];
+
+#pragma omp parallel for collapse(3) private(                                  \
+        local_left_data, local_right_data, local_left_vec1, local_left_vec2)
+  // #pragma omp parallel for collapse(3)
+  //  for (int iw = 0; iw < rank_local_num_samples_; iw++) {
+  for (int iw_b = 0; iw_b < rank_local_num_samples_; iw_b += sbs) {
+    for (int pm_b = 0; pm_b < numParams; pm_b += pbs) {
+      for (int pm2_b = 0; pm2_b < numParams; pm2_b += pbs) {
+
+        Matrix localLeft(pbs, pbs, local_left_data);
+        Matrix localRight(pbs, pbs, local_right_data);
+        for (int i = 0; i < pbs; i++) {
+          local_left_vec1[i] = 0.0;
+          local_left_vec2[i] = 0.0;
+        }
+        for (int i = 0; i < pbs * pbs; i++) {
+          local_left_data[i] = 0.0;
+          local_right_data[i] = 0.0;
+        }
+        for (int iw = iw_b; iw < std::min(rank_local_num_samples_, iw_b + sbs);
+             iw++) {
+          Return_rt weight = RecordsOnNode_(iw, REWEIGHT) * wgtinv;
+          Return_rt eloc_new = RecordsOnNode_(iw, ENERGY_NEW);
+          const Matrix &Dsaved = DerivRecords_;
+          const Matrix &HDsaved = HDerivRecords_;
+
+          for (int pm = pm_b; pm < std::min(numParams, pm_b + pbs); pm++) {
+            Return_t wfe =
+                (HDsaved(iw, pm) + (Dsaved(iw, pm) - D_avg[pm]) * eloc_new) *
+                weight;
+            Return_t wfd = (Dsaved(iw, pm) - D_avg[pm]) * weight;
+            Return_t vterm = HDsaved(iw, pm) * (eloc_new - curAvg_w) +
+                             (Dsaved(iw, pm) - D_avg[pm]) * eloc_new *
+                                 (eloc_new - RealType(2.0) * curAvg_w);
+            if (pm2_b == 0) {
+              //                 Variance
+              // Left(0, pm + 1) += b2 * std::real(vterm) * weight;
+              // local_left_vec1[pm - pm_b] += b2 * std::real(vterm) * weight;
+              // Left(pm + 1, 0) += b2 * std::real(vterm) * weight;
+              // local_left_vec2[pm - pm_b] += b2 * std::real(vterm) * weight;
+              //                 Hamiltonian
+              // Left(0, pm + 1) += (1 - b2) * std::real(wfe);
+              local_left_vec1[pm - pm_b] += (1 - b2) * std::real(wfe);
+              // Left(pm + 1, 0) += (1 - b2) * std::real(wfd) * eloc_new;
+              local_left_vec2[pm - pm_b] +=
+                  (1 - b2) * std::real(wfd) * eloc_new;
+            }
+            for (int pm2 = pm2_b; pm2 < std::min(numParams, pm2_b + pbs);
+                 pm2++) {
+              //                Hamiltonian
+              // Left(pm + 1, pm2 + 1) +=
+              localLeft(pm - pm_b, pm2 - pm2_b) +=
+                  std::real((1 - b2) * std::conj(wfd) *
+                            (HDsaved(iw, pm2) +
+                             (Dsaved(iw, pm2) - D_avg[pm2]) * eloc_new));
+              //                Overlap
+              RealType ovlij =
+                  std::real(std::conj(wfd) * (Dsaved(iw, pm2) - D_avg[pm2]));
+              // Right(pm + 1, pm2 + 1) += ovlij;
+              localRight(pm - pm_b, pm2 - pm2_b) += ovlij;
+              //                Variance
+              RealType varij =
+                  weight *
+                  std::real(
+                      (HDsaved(iw, pm) -
+                       RealType(2.0) * std::conj(Dsaved(iw, pm) - D_avg[pm]) *
+                           eloc_new) *
+                      (HDsaved(iw, pm2) - RealType(2.0) *
+                                              (Dsaved(iw, pm2) - D_avg[pm2]) *
+                                              eloc_new));
+              // Left(pm + 1, pm2 + 1) += b2 * (varij + V_avg * ovlij);
+              localLeft(pm - pm_b, pm2 - pm2_b) += b2 * (varij + V_avg * ovlij);
+            }
+          }
+        }
+
+#if 1
+#pragma omp critical
+        for (int pm = pm_b; pm < std::min(numParams, pm_b + pbs); pm++) {
+          Left(0, pm + 1) += local_left_vec1[pm - pm_b];
+          Left(pm + 1, 0) += local_left_vec2[pm - pm_b];
+          for (int pm2 = pm2_b; pm2 < std::min(numParams, pm2_b + pbs); pm2++) {
+            Left(pm + 1, pm2 + 1) += localLeft(pm - pm_b, pm2 - pm2_b);
+            Right(pm + 1, pm2 + 1) += localRight(pm - pm_b, pm2 - pm2_b);
+          }
+        }
+#endif
+      }
+    }
+  }
+  // myComm->allreduce(Right);
+  // myComm->allreduce(Left);
+  Left(0, 0) = (1 - b2) * curAvg_w + b2 * V_avg;
+  Right(0, 0) = 1.0;
+}
 
 void fillOverlapHamiltonianMatrices(const int nsamples, const int numParams,
                                     std::vector<double> SumValue,
@@ -284,9 +424,16 @@ int main(int argc, char **argv) {
   std::chrono::system_clock::time_point fill_start =
       std::chrono::system_clock::now();
 
+#if 0
   fillOverlapHamiltonianMatrices(nsamples, nparam, SumValue, RecordsOnNode,
                                  DerivRecords, HDerivRecords, w_beta, ham,
                                  overlap);
+#endif
+#if 1
+  fillOverlapHamiltonianMatricesBlocked(nsamples, nparam, SumValue,
+                                        RecordsOnNode, DerivRecords,
+                                        HDerivRecords, w_beta, ham, overlap);
+#endif
 
   std::chrono::system_clock::time_point fill_end =
       std::chrono::system_clock::now();
@@ -303,12 +450,12 @@ int main(int argc, char **argv) {
     assert(gold_ham.rows() == ham.rows());
     assert(gold_ham.cols() == ham.cols());
 
-    double tol = 1e-5;
+    double tol = 1.0e-5;
     int nerror = 0;
     for (int i = 0; i < nparam; i++) {
       for (int j = 0; j < nparam; j++) {
         double diff = gold_overlap(i, j) - overlap(i, j);
-        if (diff > tol) {
+        if (std::abs(diff) > tol) {
           nerror++;
           if (nerror < 10) {
             std::cout << "Overlap difference " << i << " " << j << " "
@@ -327,7 +474,7 @@ int main(int argc, char **argv) {
     for (int i = 0; i < nparam; i++) {
       for (int j = 0; j < nparam; j++) {
         double diff = gold_ham(i, j) - ham(i, j);
-        if (diff > tol) {
+        if (std::abs(diff) > tol) {
           nerror++;
           if (nerror < 10) {
             std::cout << "Hamiltonian difference " << i << " " << j << " "
